f0e6180ee5e3c10e12b6ca78708af80969bb33ce
ゆず <f.toiro728@gmail.com>
2026-05-10 13:27:32 +0900

    Update excluded files in load_cogs function

--- main.py
+++ main.py
@@ -13,20 +13,24 @@ intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='$', intents=intents, help_command=None, owner_id=owner_id)
 
 async def load_cogs():
+    # 読み込みから除外するファイル
+    exclude_files = ("__init__.py", "nyanko_editor.py", "db.py")
+    
     for filename in os.listdir("./Cogs"):
-        if filename.endswith(".py") and filename not in ("__init__.py", "nyanko_editor.py"):
+        if filename.endswith(".py") and filename not in exclude_files:
             try:
                 await bot.load_extension(f"Cogs.{filename[:-3]}")
                 print(f"✅ Loaded {filename}")
             except Exception as e:
                 print(f"❌ Failed to load {filename}: {e}")
                 traceback.print_exc()
+    
     await bot.tree.sync()
     print("✅ Commands synced")
 
 bot.setup_hook = load_cogs
 
-STATUS = "❤にゃんこ大戦争自動代行❤"
+STATUS = "にゃんこ大戦争自動代行"
 
 @bot.event
 async def on_ready():
